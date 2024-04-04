9d598da9a5cb6aa209993b1af880b7aaea7474ee
Matheus Brandão Nogueura <161345684+mathiaspy22@users.noreply.github.com>
2024-04-04 02:32:11 -0300

    Add files via upload
    Aqui nesse código , o programa está lendo uma tupla com seu produto e seu respectivo preço ao lado, no final o programa retornara os produtos em uma tabela alinhada.

---
+++ Lista de preços com tuplas.py
@@ -0,0 +1,9 @@
+itens_e_preços = ('Ps5',5000,'Controle Ps5',239.90,'RTX 4060',3499.99,'Processador Intel I9',1599.40)
+print('=========== Listagem de preços =================')
+print('-------------------------------------------------------------------------------')
+print(f'{itens_e_preços[0]}........................... R${itens_e_preços[1]}')
+print(f'{itens_e_preços[2]}.................. R${itens_e_preços[3]}')
+print(f'{itens_e_preços[4]}.......................R${itens_e_preços[5]}')
+print(f'{itens_e_preços[6]}...........R$ {itens_e_preços[7]}')
+
+print('------------------------------------------------------------------------------')
