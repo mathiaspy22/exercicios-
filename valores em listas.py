3a1d52c28f20f17421de05fe7192756aa3b5bbee
Matheus Brandão Nogueura <161345684+mathiaspy22@users.noreply.github.com>
2024-04-10 09:38:36 -0300

    Add files via upload
    Apenas um estudo que estou fazendo sobre listas, esse mini programa apenas adiciona números com os métodos append e mostra as posições dos determinados números.

---
+++ valores em listas.py
@@ -0,0 +1,8 @@
+valores = list()
+valores.append(5)
+valores.append(4)
+valores.append(3)
+print(valores)
+for posição_valores ,valor in enumerate(valores):
+    print(f' na posição {posição_valores} o valor é {valor}')
+print('Cheguei no fim do programa..')
