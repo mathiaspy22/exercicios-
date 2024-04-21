8dd0625d0cfb6088ae7bb93b983ad735f3494d07
Matheus Brandão Nogueura <161345684+mathiaspy22@users.noreply.github.com>
2024-04-20 23:32:28 -0300

    Add files via upload
    Neste programa , estão localizados em uma lista, vários itens com seus respectivos preços ao lado e ao inicializar o programa, o programa retornara uma tabela com o preço separado em uma tabela.

---
+++ listagem de preços.py
@@ -0,0 +1,10 @@
+print('---------------------- Listagem de preços -------------------')
+
+produtos_e_preços =('Headset',129.99,
+                    'Ps5',4999.90,
+                    'Luva',23.30,
+                    'Bota',30.22)
+print(f'{produtos_e_preços[0]}.........R$ {produtos_e_preços[1]}')
+print(f'{produtos_e_preços[2]}.............R$ {produtos_e_preços[3]}')
+print(f'{produtos_e_preços[4]}............R$ {produtos_e_preços[5]}')
+print(f'{produtos_e_preços[6]}............R$ {produtos_e_preços[7]}')
